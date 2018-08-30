9b96baf42c6db9656ddb6c1e7d3c22cac7bcd1b8
unknown <amansahota1996@gmail.com>
2018-08-30 19:14:02 +0530

    Add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('new user connected');
 
+    socket.emit('newMessage',{
+        from:'Itachi',
+        text:'True shinobi do not seek glory, they protect from the shadows',
+    });
+
+    socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+    });
+
     socket.on('disconnect',()=>{
         console.log('user was disconnected');
     });
